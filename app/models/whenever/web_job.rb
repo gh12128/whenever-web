e5a95e420dfdc9a596b1026f5777d7757fe5a052
gh <guohang12138@gmail.com>
2018-10-31 15:13:49 +0800

    update job list

--- app/models/whenever/web_job.rb
+++ app/models/whenever/web_job.rb
@@ -10,24 +10,42 @@ module Whenever
       @interval = interval
     end
 
-    delegate :output, :at, to: :@job
+    # delegate :output, :at, to: :@job
 
     def task
-      @job.options[:task]
+      @job[1][0].options[:task]
     end
 
     def description
-      @job.options.fetch(:description) { task }
+      # @job.options.fetch(:description) { task }
+      if @job[1][0].options[:description].present?
+        @job[1][0].options[:description]
+      else
+        task
+      end
     end
 
     def interval
-      distance_of_time_in_words(@interval).gsub('about', '')
+      distance_of_time_in_words(@job[0])#.gsub('about', '')
     rescue ArgumentError
       @interval
     end
 
     def roles
-      @job.roles.join(', ')
+      @job[1][0].roles.join(', ')
     end
+
+    def at
+      @job[1][0].at
+    end
+
+    def to
+      @job[1][0].at
+    end
+
+    def output
+      @job[1][0].options[:output]
+    end
+
   end
 end
